249a2633ea651ef8ef674d2237dc18483654e51f
quaz4 <hmaswill@gmail.com>
2017-01-26 22:38:12 +0800

    Built basic RecyclerView with CardView. Not tested yet, requires List<Background> building first.

---
+++ mobile/src/main/java/net/dust_bowl/togetheragain/Background.java
@@ -0,0 +1,13 @@
+package net.dust_bowl.togetheragain;
+
+import android.widget.TextView;
+
+/**
+ * Created by quaz4 on 26/01/2017.
+ */
+
+public class Background
+{
+    String dateTime;
+    int backgroundId;
+}
